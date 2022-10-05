f6acea8e0fee4d161b4edbcd64ecdaea1140c416
Luke Barkess <luke.barkess@icloud.com>
2022-10-05 12:19:17 +0100

    i

--- Adder/Program.cs
+++ Adder/Program.cs
@@ -13,14 +13,14 @@ namespace Adder
             Console.WriteLine("Adder:\n");
 
             Console.Write("Enter number 1: ");
-            int num1 = int.Parse(Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
 
             Console.Write("Enter number 2: ");
-            int num2 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
 
-            int result = num1 + num2;
+            int sum = number1 + number2;
 
-            Console.WriteLine($"Result: {result}");
+            Console.WriteLine($"Result: {sum}");
 
             Console.ReadLine();
         }
